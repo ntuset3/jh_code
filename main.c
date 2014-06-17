cd92cc1eb46a4412c9e37950373157cd300bdf90
dani-DAM1 <picamoixonenc@gmail.com>
2014-06-18 01:59:44 +0200

    *

--- main.c
+++ main.c
@@ -1,34 +1,35 @@
 #include<stdio.h>
 
 void inicialitzaTauler();
-void mostrar();
+void mostraTauler();
 void introPosicio();
 void comprovaGuanyador();
 void moureFitxa();
 
-int tauler[3][3];
-int jugador=2,tirades=1,guanyador=0;
+int tauler[3][3],tirades=1,guanyador=0;
 
 int main(void){
-
+    int jugador=2
     inicialitzaTauler();
-    mostrar();
+    mostraTauler();
     tabler[1][1]=2;
     do{
-        introPosicio();
+        introPosicio1();
         if(jugador==1){
             matriu[posx][posy]=2;
         }
         else{
             matriu[posx][posy]=1;
         }
-        mostrar();
+        tirades++;
+        mostraTauler();
         comprovaGuanyador();
     }
     while(tirades<6||guanyador==0);
     while(guanyador==0){
-        introPosicio();
+        introPosicio2();
         moureFitxa();
+        mostraTauler();
         comprovaGuanyador();
     }
     if(jugador==1){
